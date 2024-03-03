ecfeb5a9ca512d6b822052d3b766b017657e4144
Michal Boguszewski <michal1842001@gmail.com>
2024-03-03 19:12:57 +0100

    Add egui integration

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #[macro_use]
 extern crate glium;
-use glium::Surface;
+use glium::{DrawParameters, Surface};
+use winit::{event, event_loop};
 
 fn main() {
+    let mut width = 800;
+    let mut height = 600;
+    
     let event_loop = winit::event_loop::EventLoopBuilder::new()
-        .build()
-        .expect("event loop building");
-    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
-        .with_title("Glium tutorial #2")
+        .build();
+    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+        .with_title("CAD")
+        .with_inner_size(width, height)
         .build(&event_loop);
+
+    let mut egui_glium = egui_glium::EguiGlium::new(&display, &window, &event_loop);
 
     #[derive(Copy, Clone)]
     struct Vertex {
@@ -16,9 +22,9 @@ fn main() {
     }
     implement_vertex!(Vertex, position);
     let shape = vec![
-        Vertex { position: [-0.5, -0.5] },
-        Vertex { position: [ 0.0,  0.5] },
-        Vertex { position: [ 0.5, -0.25] }
+        Vertex { position: [-1.0, -1.0] },
+        Vertex { position: [ 0.0,  1.0] },
+        Vertex { position: [ 1.0, -0.25] }
     ];
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
@@ -51,15 +57,80 @@ fn main() {
                 &Default::default()).unwrap();
     target.finish().unwrap();
 
-    event_loop.run(move |ev, window_target| {
-        match ev {
-            winit::event::Event::WindowEvent { event, .. } => match event {
-                winit::event::WindowEvent::CloseRequested => {
-                    window_target.exit();
-                },
-                _ => (),
-            },
+    event_loop.run(move |event, _window_target, control_flow| {
+        let mut redraw = || {
+            let mut quit = false;
+
+            let repaint_after = egui_glium.run(&window, |egui_ctx| {
+                egui::SidePanel::left("side_panel").exact_width(183.0).show(egui_ctx, |ui| {
+                    ui.heading("Hello World!");
+                    if ui.button("Quit").clicked() {
+                        quit = true;
+                    }
+                });
+            });
+
+            *control_flow = if quit {
+                event_loop::ControlFlow::Exit
+            } else if repaint_after.is_zero() {
+                window.request_redraw();
+                event_loop::ControlFlow::Poll
+            } else if let Some(repaint_after_instant) =
+                std::time::Instant::now().checked_add(repaint_after)
+            {
+                event_loop::ControlFlow::WaitUntil(repaint_after_instant)
+            } else {
+                event_loop::ControlFlow::Wait
+            };
+
+            {
+                let mut target = display.draw();
+                
+                let mut drawing_parameters = DrawParameters::default();
+                drawing_parameters.viewport = Some(glium::Rect {
+                    left: 200,
+                    bottom: 0,
+                    width: width - 200,
+                    height: height,
+                });
+
+                target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+                egui_glium.paint(&display, &mut target);
+
+                target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                            &drawing_parameters).unwrap();
+
+                target.finish().unwrap();
+            }
+        };
+        match event {
+            event::Event::RedrawRequested(_) => redraw(),
+
+            event::Event::WindowEvent { event, .. } => {
+                use event::WindowEvent;
+                match &event {
+                    WindowEvent::CloseRequested | WindowEvent::Destroyed => {
+                        *control_flow = winit::event_loop::ControlFlow::Exit;
+                    }
+                    WindowEvent::Resized(new_size) => {
+                        display.resize((*new_size).into());
+                        width = new_size.width;
+                        height = new_size.height;
+                    }
+                    _ => {}
+                }
+
+                let event_response = egui_glium.on_event(&event);
+
+                if event_response.repaint {
+                    window.request_redraw();
+                }
+            }
+            event::Event::NewEvents(event::StartCause::ResumeTimeReached { .. }) => {
+                window.request_redraw();
+            }
             _ => (),
         }
-    }).unwrap();
+    });
 }
