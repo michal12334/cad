8388f318b3a89cb95624f4bb9430b0975f28844f
Michal Boguszewski <michal1842001@gmail.com>
2024-03-03 14:02:52 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+#[macro_use]
+extern crate glium;
+use glium::Surface;
+
+fn main() {
+    let event_loop = winit::event_loop::EventLoopBuilder::new()
+        .build()
+        .expect("event loop building");
+    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+        .with_title("Glium tutorial #2")
+        .build(&event_loop);
+
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    implement_vertex!(Vertex, position);
+    let shape = vec![
+        Vertex { position: [-0.5, -0.5] },
+        Vertex { position: [ 0.0,  0.5] },
+        Vertex { position: [ 0.5, -0.25] }
+    ];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.0, 1.0, 1.0);
+    target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                &Default::default()).unwrap();
+    target.finish().unwrap();
+
+    event_loop.run(move |ev, window_target| {
+        match ev {
+            winit::event::Event::WindowEvent { event, .. } => match event {
+                winit::event::WindowEvent::CloseRequested => {
+                    window_target.exit();
+                },
+                _ => (),
+            },
+            _ => (),
+        }
+    }).unwrap();
+}
