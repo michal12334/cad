f4108fc40347653e97822b571a94dd348a623244
Michal Boguszewski <michal1842001@gmail.com>
2024-03-15 22:56:25 +0100

    Remove meshes and transformers from storage

--- src/backend/src/data_access/storage.rs
+++ src/backend/src/data_access/storage.rs
@@ -5,16 +5,12 @@ use crate::domain::transformer::Transformer;
 
 pub struct Storage {
     pub toruses: HashMap<u64, Torus>,
-    pub transformers: HashMap<u64, Transformer>,
-    pub meshes: HashMap<u64, Mesh>,
 }
 
 impl Storage {
     pub fn new() -> Self {
         Self {
             toruses: HashMap::new(),
-            transformers: HashMap::new(),
-            meshes: HashMap::new(),
         }
     }
 }
