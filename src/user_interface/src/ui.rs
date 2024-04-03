4b194e0acc95d8bde42ab8a435fb7ebb9f26ebfc
Michal Boguszewski <michal1842001@gmail.com>
2024-04-03 22:25:51 +0200

    Refactor bulding transformation panel

--- src/user_interface/src/ui.rs
+++ src/user_interface/src/ui.rs
@@ -9,13 +9,13 @@ use backend::cqrs::cursors::cursor_details::CursorDTO;
 use backend::cqrs::cursors::transform_cursor::TransformCursor;
 use backend::cqrs::points::add_point::AddPoint;
 use backend::cqrs::points::all_points::AllPoints;
-use backend::cqrs::points::point_details::{LittleTransformerDTO, PointDetails};
+use backend::cqrs::points::point_details::{LittleTransformerDTO, PointDetails, PointDTO};
 use backend::cqrs::points::rename_point::RenamePoint;
 use backend::cqrs::points::transform_point::TransformPoint;
 use backend::cqrs::toruses::add_torus::AddTorus;
 use backend::cqrs::toruses::all_toruses::AllToruses;
 use backend::cqrs::toruses::rename_torus::RenameTorus;
-use backend::cqrs::toruses::torus_details::{TorusDetails, TransformerDTO};
+use backend::cqrs::toruses::torus_details::{TorusDetails, TorusDTO, TransformerDTO};
 use backend::cqrs::toruses::transform_torus::TransformTours;
 use backend::cqrs::toruses::update_torus::UpdateTorus;
 
@@ -178,166 +178,176 @@ impl Ui {
         Resize::default().id_source("resize_2").show(ui, |ui| {
             ScrollArea::vertical().id_source("a2").show(ui, |ui| {
                 if self.cursor_selected {
-                    let cursor = self.cursor.as_mut().unwrap();
-
-                    let transformer_sliders = vec![
-                        Slider::new(&mut cursor.transformer.position.0, -5.0..=5.0)
-                            .text("position X")
-                            .ui(ui),
-                        Slider::new(&mut cursor.transformer.position.1, -5.0..=5.0)
-                            .text("position Y")
-                            .ui(ui),
-                        Slider::new(&mut cursor.transformer.position.2, -5.0..=5.0)
-                            .text("position Z")
-                            .ui(ui),
-                    ];
-
-                    if transformer_sliders.iter().any(|f| f.changed()) {
-                        cqrs.execute(&TransformCursor {
-                            transformer: LittleTransformerDTO {
-                                position: cursor.transformer.position,
-                            },
-                        });
-                    }
-
-                    return;
-                }
-
-                if self.selected_objects.len() != 1 {
-                    return;
-                }
-
-                let object = self
-                    .objects
-                    .iter_mut()
-                    .find(|t| t.get_id() == self.selected_objects[0].get_id())
-                    .unwrap();
-                match object {
-                    Torus(torus) => {
-                        if ui.text_edit_singleline(&mut torus.name).lost_focus() {
-                            cqrs.execute(&RenameTorus {
-                                id: torus.id,
-                                name: torus.name.clone(),
-                            });
-                            *torus = cqrs.get(&TorusDetails { id: torus.id });
-                        }
-
-                        let torus_sliders = vec![
-                            Slider::new(&mut torus.major_radius, 0.01..=5.0)
-                                .text("major radius")
-                                .ui(ui),
-                            Slider::new(&mut torus.minor_radius, 0.01..=5.0)
-                                .text("minor radius")
-                                .ui(ui),
-                            Slider::new(&mut torus.major_segments, 1..=1000)
-                                .text("major segments")
-                                .ui(ui),
-                            Slider::new(&mut torus.minor_segments, 1..=1000)
-                                .text("minor segments")
-                                .ui(ui),
-                        ];
-
-                        if torus_sliders.iter().any(|f| f.changed()) {
-                            cqrs.execute(&UpdateTorus {
-                                id: torus.id,
-                                major_radius: torus.major_radius,
-                                minor_radius: torus.minor_radius,
-                                major_segments: torus.major_segments,
-                                minor_segments: torus.minor_segments,
-                            });
-                            *torus = cqrs.get(&TorusDetails { id: torus.id });
-                        }
-
-                        let transformer_sliders = vec![
-                            Slider::new(&mut torus.transformer.position.0, -5.0..=5.0)
-                                .text("position X")
-                                .ui(ui),
-                            Slider::new(&mut torus.transformer.position.1, -5.0..=5.0)
-                                .text("position Y")
-                                .ui(ui),
-                            Slider::new(&mut torus.transformer.position.2, -5.0..=5.0)
-                                .text("position Z")
-                                .ui(ui),
-                            Slider::new(&mut torus.transformer.scale.0, 0.1..=5.0)
-                                .text("scale X")
-                                .ui(ui),
-                            Slider::new(&mut torus.transformer.scale.1, 0.1..=5.0)
-                                .text("scale Y")
-                                .ui(ui),
-                            Slider::new(&mut torus.transformer.scale.2, 0.1..=5.0)
-                                .text("scale Z")
-                                .ui(ui),
-                            Slider::new(
-                                &mut torus.transformer.rotation.0,
-                                -1.0..=1.0,
-                            )
-                            .text("rotation X")
-                            .ui(ui),
-                            Slider::new(
-                                &mut torus.transformer.rotation.1,
-                                -1.0..=1.0,
-                            )
-                            .text("rotation Y")
-                            .ui(ui),
-                            Slider::new(
-                                &mut torus.transformer.rotation.2,
-                                -1.0..=1.0,
-                            )
-                            .text("rotation Z")
-                            .ui(ui),
-                            Slider::new(
-                                &mut torus.transformer.rotation.3,
-                                -1.0..=1.0,
-                            )
-                            .text("rotation W")
-                            .ui(ui),
-                        ];
-
-                        if transformer_sliders.iter().any(|f| f.changed()) {
-                            cqrs.execute(&TransformTours {
-                                id: torus.id,
-                                transformer: TransformerDTO {
-                                    position: torus.transformer.position,
-                                    rotation: torus.transformer.rotation,
-                                    scale: torus.transformer.scale,
-                                },
-                            });
-                            *torus = cqrs.get(&TorusDetails { id: torus.id });
-                        }
-                    }
-                    Point(point) => {
-                        if ui.text_edit_singleline(&mut point.name).lost_focus() {
-                            cqrs.execute(&RenamePoint {
-                                id: point.id,
-                                name: point.name.clone(),
-                            });
-                            *point = cqrs.get(&PointDetails { id: point.id });
-                        }
-
-                        let transformer_sliders = vec![
-                            Slider::new(&mut point.transformer.position.0, -5.0..=5.0)
-                                .text("position X")
-                                .ui(ui),
-                            Slider::new(&mut point.transformer.position.1, -5.0..=5.0)
-                                .text("position Y")
-                                .ui(ui),
-                            Slider::new(&mut point.transformer.position.2, -5.0..=5.0)
-                                .text("position Z")
-                                .ui(ui),
-                        ];
-
-                        if transformer_sliders.iter().any(|f| f.changed()) {
-                            cqrs.execute(&TransformPoint {
-                                id: point.id,
-                                transformer: LittleTransformerDTO {
-                                    position: point.transformer.position,
-                                },
-                            });
-                            *point = cqrs.get(&PointDetails { id: point.id });
-                        }
-                    }
+                    self.build_cursor_transformation_panel(ui, cqrs);
+                } else if self.selected_objects.len() == 1 {
+                    self.build_single_object_transformation_panel(ui, cqrs);
                 }
             });
         });
+    }
+
+    fn build_cursor_transformation_panel(&mut self, ui: &mut egui::Ui, cqrs: &mut CQRS) {
+        let cursor = self.cursor.as_mut().unwrap();
+
+        let transformer_sliders = vec![
+            Slider::new(&mut cursor.transformer.position.0, -5.0..=5.0)
+                .text("position X")
+                .ui(ui),
+            Slider::new(&mut cursor.transformer.position.1, -5.0..=5.0)
+                .text("position Y")
+                .ui(ui),
+            Slider::new(&mut cursor.transformer.position.2, -5.0..=5.0)
+                .text("position Z")
+                .ui(ui),
+        ];
+
+        if transformer_sliders.iter().any(|f| f.changed()) {
+            cqrs.execute(&TransformCursor {
+                transformer: LittleTransformerDTO {
+                    position: cursor.transformer.position,
+                },
+            });
+        }
+    }
+    
+    fn build_single_object_transformation_panel(&mut self, ui: &mut egui::Ui, cqrs: &mut CQRS) {
+        let object = self
+            .objects
+            .iter_mut()
+            .find(|t| t.get_id() == self.selected_objects[0].get_id())
+            .unwrap();
+        match object {
+            Torus(torus) => {
+                Ui::build_torus_transformation_panel(ui, cqrs, torus);
+            }
+            Point(point) => {
+                Ui::build_point_transformation_panel(ui, cqrs, point);
+            }
+        }
+    }
+
+    fn build_torus_transformation_panel(ui: &mut egui::Ui, cqrs: &mut CQRS, torus: &mut TorusDTO) {
+        if ui.text_edit_singleline(&mut torus.name).lost_focus() {
+            cqrs.execute(&RenameTorus {
+                id: torus.id,
+                name: torus.name.clone(),
+            });
+            *torus = cqrs.get(&TorusDetails { id: torus.id });
+        }
+
+        let torus_sliders = vec![
+            Slider::new(&mut torus.major_radius, 0.01..=5.0)
+                .text("major radius")
+                .ui(ui),
+            Slider::new(&mut torus.minor_radius, 0.01..=5.0)
+                .text("minor radius")
+                .ui(ui),
+            Slider::new(&mut torus.major_segments, 1..=1000)
+                .text("major segments")
+                .ui(ui),
+            Slider::new(&mut torus.minor_segments, 1..=1000)
+                .text("minor segments")
+                .ui(ui),
+        ];
+
+        if torus_sliders.iter().any(|f| f.changed()) {
+            cqrs.execute(&UpdateTorus {
+                id: torus.id,
+                major_radius: torus.major_radius,
+                minor_radius: torus.minor_radius,
+                major_segments: torus.major_segments,
+                minor_segments: torus.minor_segments,
+            });
+            *torus = cqrs.get(&TorusDetails { id: torus.id });
+        }
+
+        let transformer_sliders = vec![
+            Slider::new(&mut torus.transformer.position.0, -5.0..=5.0)
+                .text("position X")
+                .ui(ui),
+            Slider::new(&mut torus.transformer.position.1, -5.0..=5.0)
+                .text("position Y")
+                .ui(ui),
+            Slider::new(&mut torus.transformer.position.2, -5.0..=5.0)
+                .text("position Z")
+                .ui(ui),
+            Slider::new(&mut torus.transformer.scale.0, 0.1..=5.0)
+                .text("scale X")
+                .ui(ui),
+            Slider::new(&mut torus.transformer.scale.1, 0.1..=5.0)
+                .text("scale Y")
+                .ui(ui),
+            Slider::new(&mut torus.transformer.scale.2, 0.1..=5.0)
+                .text("scale Z")
+                .ui(ui),
+            Slider::new(
+                &mut torus.transformer.rotation.0,
+                -1.0..=1.0,
+            )
+                .text("rotation X")
+                .ui(ui),
+            Slider::new(
+                &mut torus.transformer.rotation.1,
+                -1.0..=1.0,
+            )
+                .text("rotation Y")
+                .ui(ui),
+            Slider::new(
+                &mut torus.transformer.rotation.2,
+                -1.0..=1.0,
+            )
+                .text("rotation Z")
+                .ui(ui),
+            Slider::new(
+                &mut torus.transformer.rotation.3,
+                -1.0..=1.0,
+            )
+                .text("rotation W")
+                .ui(ui),
+        ];
+
+        if transformer_sliders.iter().any(|f| f.changed()) {
+            cqrs.execute(&TransformTours {
+                id: torus.id,
+                transformer: TransformerDTO {
+                    position: torus.transformer.position,
+                    rotation: torus.transformer.rotation,
+                    scale: torus.transformer.scale,
+                },
+            });
+            *torus = cqrs.get(&TorusDetails { id: torus.id });
+        }
+    }
+    
+    fn build_point_transformation_panel(ui: &mut egui::Ui, cqrs: &mut CQRS, point: &mut PointDTO) {
+        if ui.text_edit_singleline(&mut point.name).lost_focus() {
+            cqrs.execute(&RenamePoint {
+                id: point.id,
+                name: point.name.clone(),
+            });
+            *point = cqrs.get(&PointDetails { id: point.id });
+        }
+
+        let transformer_sliders = vec![
+            Slider::new(&mut point.transformer.position.0, -5.0..=5.0)
+                .text("position X")
+                .ui(ui),
+            Slider::new(&mut point.transformer.position.1, -5.0..=5.0)
+                .text("position Y")
+                .ui(ui),
+            Slider::new(&mut point.transformer.position.2, -5.0..=5.0)
+                .text("position Z")
+                .ui(ui),
+        ];
+
+        if transformer_sliders.iter().any(|f| f.changed()) {
+            cqrs.execute(&TransformPoint {
+                id: point.id,
+                transformer: LittleTransformerDTO {
+                    position: point.transformer.position,
+                },
+            });
+            *point = cqrs.get(&PointDetails { id: point.id });
+        }
     }
 }
