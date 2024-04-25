13e2f80986367498ffdb569b69db084e3324f908
Michal Boguszewski <michal1842001@gmail.com>
2024-04-25 15:17:49 +0200

    Draw bezier of any size

--- src/bezier_c0_drawer.rs
+++ src/bezier_c0_drawer.rs
@@ -29,21 +29,52 @@ impl BezierC0Drawer {
             layout(lines_adjacency) in;
             layout(line_strip, max_vertices = 101) out;
             
+            uniform int mode;
+            uniform int number_of_primitives;
+            
             void main() {
-                for(float t = 0.0; t <= 1.00; t += 0.01) {
-                    float it = 1.0 - t;
-                    float b0 = it * it * it;
-                    float b1 = 3.0 * it * it * t;
-                    float b2 = 3.0 * it * t * t;
-                    float b3 = t * t * t;
-                    
-                    vec4 position = 
-                        gl_in[0].gl_Position * b0 
-                        + gl_in[1].gl_Position * b1 
-                        + gl_in[2].gl_Position * b2
-                        + gl_in[3].gl_Position * b3;
-                    gl_Position = position;
-                    EmitVertex();
+                if (mode == 0 && gl_PrimitiveIDIn == number_of_primitives - 1) {
+                    for (float t = 0.0; t <= 1.00; t += 0.01) {
+                        float it = 1.0 - t;
+                        float b0 = it * it;
+                        float b1 = 2.0 * it * t;
+                        float b2 = t * t;
+                        
+                        vec4 position = 
+                            gl_in[0].gl_Position * b0 
+                            + gl_in[1].gl_Position * b1 
+                            + gl_in[2].gl_Position * b2;
+                        gl_Position = position;
+                        EmitVertex();
+                    }
+                } else if (mode == 2 && gl_PrimitiveIDIn == number_of_primitives - 1) {
+                    for (float t = 0.0; t <= 1.00; t += 0.01) {
+                        float it = 1.0 - t;
+                        float b0 = it;
+                        float b1 = t;
+                        
+                        vec4 position = 
+                            gl_in[0].gl_Position * b0 
+                            + gl_in[1].gl_Position * b1;
+                        gl_Position = position;
+                        EmitVertex();
+                    }
+                } else {
+                    for (float t = 0.0; t <= 1.00; t += 0.01) {
+                        float it = 1.0 - t;
+                        float b0 = it * it * it;
+                        float b1 = 3.0 * it * it * t;
+                        float b2 = 3.0 * it * t * t;
+                        float b3 = t * t * t;
+                        
+                        vec4 position = 
+                            gl_in[0].gl_Position * b0 
+                            + gl_in[1].gl_Position * b1 
+                            + gl_in[2].gl_Position * b2
+                            + gl_in[3].gl_Position * b3;
+                        gl_Position = position;
+                        EmitVertex();
+                    }
                 }
                 EndPrimitive();
             }
@@ -88,10 +119,14 @@ impl BezierC0Drawer {
         view_matrix: &math::matrix4::Matrix4,
         color: [f32; 4],
     ) {
-        let points = points.iter().map(|p| {
+        let mut points = points.iter().map(|p| {
             Vertex { position: [p.transformer.position.0 as f32, p.transformer.position.1 as f32, p.transformer.position.2 as f32] }
         })
             .collect::<Vec<Vertex>>();
+        let len = points.len();
+        while points.len() % 3 != 1 {
+            points.push(Vertex { position: [0.0, 0.0, 0.0] });
+        }
         let vertex_buffer = glium::VertexBuffer::new(display, &points).unwrap();
         let indices =
             glium::IndexBuffer::new(display, glium::index::PrimitiveType::LinesListAdjacency, &(0..(points.len() as u16 - 3)).step_by(3).flat_map(|f| [f, f + 1, f + 2, f + 3]).collect::<Vec<u16>>()).unwrap();
@@ -103,7 +138,9 @@ impl BezierC0Drawer {
                 &uniform! {
                     perspective: perspective.data,
                     view: view_matrix.data,
-                    obj_color: color
+                    obj_color: color,
+                    mode: len as i32 % 3,
+                    number_of_primitives: (len as i32 - 1) / 3 + 1,
                 },
                 &self.drawing_parameters,
             )
