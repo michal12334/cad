cf34f670876a538f34eaa948b35540d223bfaa79
Michal Boguszewski <michal1842001@gmail.com>
2024-06-02 20:34:37 +0200

    Fix bezier points order

--- src/backend/src/cqrs/beziers_c0/bezier_c0_points.rs
+++ src/backend/src/cqrs/beziers_c0/bezier_c0_points.rs
@@ -12,17 +12,17 @@ impl Query<BezierC0Points, Vec<PointDTO>> for BezierC0Points {
     fn get(query: &BezierC0Points, app_state: Rc<RefCell<Backend>>) -> Vec<PointDTO> {
         let app_state = app_state.borrow();
         let bezier_c0 = app_state.storage.beziers_c0.get(&query.id).unwrap();
-        app_state
-            .storage
-            .points
-            .values()
-            .filter(|point| bezier_c0.points.iter().any(|p| point.id == p.id))
-            .map(|point| PointDTO {
-                id: point.id,
-                name: point.name.clone(),
-                transformer: LittleTransformerDTO {
-                    position: point.transformer.position,
-                },
+        bezier_c0.points
+            .iter()
+            .map(|point| {
+                let p = app_state.storage.points.get(&point.id).unwrap();
+                PointDTO {
+                    id: p.id,
+                    name: p.name.clone(),
+                    transformer: LittleTransformerDTO {
+                        position: p.transformer.position,
+                    },
+                }
             })
             .collect()
     }
