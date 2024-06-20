fab94120f591afab548a53ba210f45251f765827
Michal Boguszewski <michal1842001@gmail.com>
2024-06-20 20:07:13 +0200

    Add point to selected bezier c2

--- src/backend/src/processes/beziers_c2/mod.rs
+++ src/backend/src/processes/beziers_c2/mod.rs
@@ -1,1 +1,2 @@
 pub mod publishers;
+pub mod add_point_to_selected_beziers_c2_on_point_created;
--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ use backend::domain::transformer::LittleTransformer;
 use backend::processes::beziers_c0::add_point_to_selected_beziers_c0_on_point_created::AddPointToSelectedBeziersC0OnPointCreated;
 use backend::processes::beziers_c0::move_bezier_c0_points_on_point_moved::MoveBezierC0PointsOnPointMoved;
 use backend::processes::beziers_c0::publishers::{BezierC0CreatedPublisher, BezierC0DeletedPublisher, BezierC0PointMovedPublisher, BezierC0PointsDeletedPublisher, BezierC0RenamedPublisher, PointAddedToBezierC0Publisher};
+use backend::processes::beziers_c2::add_point_to_selected_beziers_c2_on_point_created::AddPointToSelectedBeziersC2OnPointCreated;
 use backend::processes::beziers_c2::publishers::{BezierC2CreatedPublisher, PointAddedToBezierC2Publisher};
 use infrastructure::event_bus::EventBus;
 use math::vector4::Vector4;
@@ -118,6 +119,11 @@ fn main() {
     event_bus
         .borrow_mut()
         .add_consumer(MoveBezierC0PointsOnPointMoved {
+            backend: app_state.clone(),
+        });
+    event_bus
+        .borrow_mut()
+        .add_consumer(AddPointToSelectedBeziersC2OnPointCreated {
             backend: app_state.clone(),
         });
 
